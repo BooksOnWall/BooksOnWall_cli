ed2e929b4420bb95fccf58d1c3a21970a5113b9c
Tom Bouillut <tom@croll.fr>
2019-12-13 11:05:09 -0300

    added react-native-android-location-enabler react-native-permissions && replace react-native-download by rn-fetch-blob

--- data/src/App.js
+++ data/src/App.js
@@ -14,36 +14,24 @@ import SplashScreen from 'react-native-splash-screen';
 import FirstRun from './src/api/firstRun/FirstRun';
 import Icon from 'react-native-vector-icons/Ionicons';
 import * as RNLocalize from "react-native-localize";
-
-
+import {check, request,  PERMISSIONS, RESULTS} from 'react-native-permissions';
+import RNAndroidLocationEnabler from 'react-native-android-location-enabler';
+import NetInfo from "@react-native-community/netinfo";
+import { Overlay } from 'react-native-elements';
 const MainNavigator = createStackNavigator({
   Intro: { screen: Intro},
   Stories: {screen: Stories},
-  Story: {screen: Story},
+  Story: {screen: Story}
 },
 {
+    headerMode: 'none',
     initialRouteName: 'Intro',
+    navigationOptions: {
+      header: null
+    }
+
 });
 
-// const App = createAppContainer(MainNavigator);
-//
-// const AppNavigator = createStackNavigator({
-//     Intro: { screen: Intro},
-//     AR: { screen: AR},
-//     Stories: { screen: Stories},
-//     Stages: { screen: Stages},
-//   },
-//   {
-//     initialRouteName: 'Intro',
-//     transitionConfig: () => fromRight(),
-//     swipeEnabled: true,
-//     animationEnabled: true,
-//     headerMode: 'none',
-//     lazy: true,
-//     navigationOptions: {
-//       header: null,
-//     },
-//   });
 
 const AppContainer = createAppContainer(MainNavigator);
 
@@ -60,6 +48,8 @@ export default class App extends Component {
     try {
       if(Platform.OS !== 'web') {
         await this.handleLocales();
+        //await this.networkCheck();
+        await this.checkPermissions();
         await this.loadStories();
         SplashScreen.hide();
       }
@@ -67,17 +57,47 @@ export default class App extends Component {
       console.warn(e);
     }
   }
-  async handleLocales() {
-    this.locales = RNLocalize.getLocales();
-  }
+  networkCheck = () => {
+    NetInfo.fetch().then(state => {
+      // console.warn("Connection type", state.type);
+      // console.warn("Is connected?", state.isConnected);
+      !state.isConnected ? console.error('No internet connection') : '';
 
-  getLocale() {
-    if (this.locales) {
-      if (Array.isArray(this.locales)) {
-        return this.locales[0];
-      }
-    }
-    return null;
+
+    });
+  }
+  checkPermissions = async () => {
+    request(
+      Platform.select({
+        android: PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION,
+        ios: PERMISSIONS.IOS.LOCATION_ALWAYS,
+      }),
+    );
+    request(
+      Platform.select({
+        android: PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+        ios: PERMISSIONS.IOS.LOCATION_WHEN_IN_USE,
+      }),
+    );
+    RNAndroidLocationEnabler.promptForEnableLocationIfNeeded({interval: 10000, fastInterval: 5000})
+      .then(data => {
+        // The user has accepted to enable the location services
+        // data can be :
+        //console.warn(data);
+        //  - "already-enabled" if the location services has been already enabled
+        //  - "enabled" if user has clicked on OK button in the popup
+      }).catch(err => {
+        console.warn(err);
+        // The user has not accepted to enable the location services or something went wrong during the process
+        // "err" : { "code" : "ERR00|ERR01|ERR02", "message" : "message"}
+        // codes :
+        //  - ERR00 : The user has clicked on Cancel button in the popup
+        //  - ERR01 : If the Settings change are unavailable
+        //  - ERR02 : If the popup has failed to open
+      });
+  }
+  handleLocales = async () => {
+    this.locales = RNLocalize.getLocales();
   }
   loadStories = async () => {
     try {
